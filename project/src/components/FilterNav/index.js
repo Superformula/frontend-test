1d5ddf8a53166fd87c3acaf303da512a1e54872d
James Gibson Weber <j.gibweb@gmail.com>
2018-11-04 14:23:53 -0800

    Implement Select

--- project/src/components/FilterNav/index.js
+++ project/src/components/FilterNav/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import FlatButton from "components/FlatButton";
 import CheckBox from "components/FormControls/CheckBox"
+import Select from "components/FormControls/Select"
 
 require("./main.scss");
 
@@ -10,36 +11,20 @@ const FilterNav = ({ setPrice, setOpenNow, setCategory }) => {
     <div className="filter-nav">
       <div className="controls">
         <span>Filter By:</span>
-
         <CheckBox 
           label="Open Now"
           onChange={setOpenNow}
         /> 
-        
-        <select defaultValue="" onChange={e => setPrice(e.target.value)}>
-          <option value="" disabled hidden>
-            Price
-          </option>
-          <option value={"All"}>All</option>
-          <option value={"$"}>$</option>
-          <option value={"$$"}>$$</option>
-          <option value={"$$$"}>$$$</option>
-          <option value={"$$$$"}>$$$$</option>
-        </select>
-
-        <select defaultValue="" onChange={e => setCategory(e.target.value)}>
-          <option value="" disabled hidden>
-            Categories
-          </option>
-          <option value={"Italian"}>Italian</option>
-          <option value={"Seafood"}>Seafood</option>
-          <option value={"Steakhouses"}>Steakhouses</option>
-          <option value={"Japanese"}>Japanese</option>
-          <option value={"American"}>American</option>
-          <option value={"Mexican"}>Mexican</option>
-          <option value={"Thai"}>Thai</option>
-        </select>
-
+        <Select 
+          onChange={setPrice}
+          placeholder="Price"
+          options={["All", "$", "$$", "$$$", "$$$$"]}
+        />
+        <Select 
+          onChange={setCategory}
+          placeholder="Categories"
+          options={["Italian", "Seafood", "Steakhouses", "Japanese", "American", "Mexican", "Thai"]}
+        />
       </div>
 
       <FlatButton>Clear All</FlatButton>
