5b71e2da130f84aab7cfec9f24a0900f3e20690b
James Gibson Weber <j.gibweb@gmail.com>
2018-11-04 04:12:50 -0800

    Make a graphql proxy server
    The yelp api / apollo client were sad about CORS

---
+++ project/server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+require('dotenv').config()
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/graphql", (req, res) => {
+  const body = req.body;
+
+  axios
+    .post(
+      "https://api.yelp.com/v3/graphql",
+      body,
+      {
+        headers: {
+          authorization:
+            `Bearer ${process.env.YELP_TOKEN}`
+        }
+      }
+    )
+    .then(response => {
+      // console.log("THEN: ", response.data);
+      res.send(response.data)
+    })
+    .catch(error => {
+      console.log("ERROR: ", error);
+      res.send(error)
+    });
+
+  
+});
+
+app.listen(4000);
