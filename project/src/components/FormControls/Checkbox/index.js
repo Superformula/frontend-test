bd823529ff6f5122d0998382ec07016e0abd4bf7
James Gibson Weber <j.gibweb@gmail.com>
2018-11-04 16:13:19 -0800

    Add checked

--- project/src/components/FormControls/Checkbox/index.js
+++ project/src/components/FormControls/Checkbox/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from 'prop-types';
 require("./main.scss");
 
-const CheckBox = ({ label, onChange }) => {
+const CheckBox = ({ label, onChange, checked }) => {
   return (
     <span className="checkbox-wrapper">
       <label>
         <input
           type="checkbox"
+          checked={checked}
           onChange={e => onChange(e.target.checked)}
         />
         {label}
@@ -18,7 +19,8 @@ const CheckBox = ({ label, onChange }) => {
 
 CheckBox.propTypes = {
   label: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.string.isRequired // TODO: one of, true, false
 }
 
 export default CheckBox;
