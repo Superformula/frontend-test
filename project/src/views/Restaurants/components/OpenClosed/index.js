c3394fb6a9ebbad4b3dbec3ff3b0c405ec6d4b40
James Gibson Weber <j.gibweb@gmail.com>
2018-11-05 18:37:39 -0800

    Use styled

--- project/src/views/Restaurants/components/OpenClosed/index.js
+++ project/src/views/Restaurants/components/OpenClosed/index.js
@@ -1,10 +1,26 @@
 import React from "react";
-require("./main.scss");
+import styled from "styled-components";
+
+const GreenDot = styled.div`
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background-color: rgb(36, 209, 36);
+  border-radius: 50%;
+`;
+
+const RedDot = styled.div`
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background-color: rgb(231, 31, 31);
+  border-radius: 50%;
+`;
 
 export const OpenNow = () => {
   return (
     <span>
-      <span className="green-dot" /> OPEN NOW
+      <GreenDot /> OPEN NOW
     </span>
   );
 };
@@ -12,7 +28,7 @@ export const OpenNow = () => {
 export const Closed = () => {
   return (
     <span>
-      <span className="red-dot" /> CLOSED
+      <RedDot /> CLOSED
     </span>
   );
 };
