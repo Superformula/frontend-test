ab743496333522e7003585ce17bdea55e1a2c9e3
Eric harrison <eric.h@rrison.io>
2019-02-11 17:04:41 -0500

    Bootstrap Button component, add to Filters component

---
+++ src/views/Home/Button/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./button.scss";
+
+const Button = () => {
+
+  return (
+    <button>
+      hello world
+    </button>
+  );
+};
+
+export default Button;
--- src/views/Home/Filters/index.js
+++ src/views/Home/Filters/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./filters.scss";
 
+import Button from '../Button';
+
 export default class Filters extends React.Component {
 
   render() {
     return (
       <div id="filters">
-        <div>filter1</div>
-        <div>filter2</div>
-        <div>filter3</div>
+        <div className="controls">
+          <div>filter1</div>
+          <div>filter2</div>
+          <div>filter3</div>
+        </div>
+        <div className="actions">
+        <Button />
+        </div>
       </div>
     );
   }
