1d2e30a95e477d83205f665720fc08b6c023ce66
Eric harrison <eric.h@rrison.io>
2019-02-11 15:17:53 -0500

    add babel loader and get scss loader working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('bla');
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.scss";
+
+let Test = () => {
+  return <h1>Test</h1>
+}
+ReactDOM.render(
+  <Test/>,
+  document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,23 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname,'src')
   },
+  module:       {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname,'src','index.html')
