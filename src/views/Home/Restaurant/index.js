a5bf4dd5dd4ab4447f9b3525aa232452a5cc9804
Eric harrison <eric.h@rrison.io>
2019-02-12 16:34:57 -0500

    improve photo css and add some restaurant bindings

--- src/views/Home/Restaurant/index.js
+++ src/views/Home/Restaurant/index.js
@@ -4,30 +4,26 @@ import "./restaurant.scss";
 import Button from "../Button";
 
 export default class Restaurant extends React.Component {
-  
-
   render() {
     const { restaurant } = this.props;
-    
+
     return (
       <div className="restaurant">
-        <img className="photo" src="https://placehold.it/300x200" alt="a placeholder image"/>
-        <h3>
-          {restaurant.name}
-        </h3>
-        <div>
-          *****
+        <div className="photo-container">
+          <img
+            className="photo"
+            src={restaurant.photos[0]}
+            alt="a placeholder image"
+          />
         </div>
+
+        <h3>{restaurant.name}</h3>
+        <div>{restaurant.rating}</div>
         <div className="detail">
-          <div>
-          Category - $$$$
-          </div>
-          <div>
-            OPEN NOW
-          </div>
+          <div>Category - {restaurant.price}</div>
+          <div>OPEN NOW</div>
         </div>
         <Button to={`/detail/${restaurant.id}`}>LEARN MORE</Button>
-        
       </div>
     );
   }
