93064c69d826c3a4275c4bd26fbcf11c6aa4ee1c
Genevieve Bulger <gbulger@ofiglobal.com>
2019-05-01 16:28:18 -0600

    Refactor restaurant container to be pure functional component

--- src/js/components/RestaurantsContainer.jsx
+++ src/js/components/RestaurantsContainer.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Filter from './Filter.jsx';
 import Results from './Results.jsx';
+
 import '../../css/main.scss';
 
-class RestaurantsContainer extends Component {
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className="grid-container restaurants-container">
-          <div className="row">
-              <div className="col-6">
-                <h1>Restaurants</h1>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
-              </div>
+function RestaurantsContainer() {
+  return (
+    <React.Fragment>
+      <div className="grid-container restaurants-container">
+        <div className="row">
+          <div className="col-6">
+            <h1>Restaurants</h1>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.
+            </h3>
           </div>
         </div>
-        <Filter />
-        <Results />
-      </React.Fragment>
-    );
-  }
+      </div>
+      <Filter />
+      <Results />
+    </React.Fragment>
+  );
 }
 
 export default RestaurantsContainer;
