bf4fbf65e75d0d713dff9f31134298d75ec5992e
Genevieve Bulger <gbulger@ofiglobal.com>
2019-05-03 10:46:40 -0600

    Adding in test coverage and first test for OpenClosed Component

--- src/js/components/OpenClosedIndicator.test.jsx
+++ src/js/components/OpenClosedIndicator.test.jsx
@@ -5,8 +5,22 @@ import OpenClosedIndicator from './OpenClosedIndicator.jsx';
 
 describe('OpenClosedIndicator', () => {
   it('should render correctly', () => {
-    const component = shallow(<OpenClosedIndicator />);
+    const component = shallow(<OpenClosedIndicator closed />);
 
     expect(component).toMatchSnapshot();
+  });
+
+  it('should render open class when closed is false', () => {
+    const component = shallow(<OpenClosedIndicator closed={ false } />),
+      indicator = component.find('.indicator');
+
+    expect(indicator.hasClass('open')).toBeTruthy();
+  });
+
+  it('should render closed class when closed is true', () => {
+    const component = shallow(<OpenClosedIndicator closed />),
+      indicator = component.find('.indicator');
+
+    expect(indicator.hasClass('closed')).toBeTruthy();
   });
 });
