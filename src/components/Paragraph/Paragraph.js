128a5a41cccf7b7088d4f2bdb56ed5bd7570b0e7
Matt Nihiser <mattnihiser@gmail.com>
2019-09-28 10:18:56 -0400

    Updated Paragraph Component, styles
    fixed import, proptype to string, cleanup return.

--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -1,18 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import './Paragraph.scss'
-import PropTypes from 'prop-types';
+import "./Paragraph.scss";
+import PropTypes from "prop-types";
 
-function Paragraph(props) {
-  return (
-    <p className='paragraph'>
-      {props.children}
-    </p>
-  );
-}
+const Paragraph = props => <p className="paragraph">{props.children}</p>;
 
 Paragraph.propTypes = {
-  children: PropTypes.object
-}
+  children: PropTypes.string
+};
 
 export default Paragraph;
