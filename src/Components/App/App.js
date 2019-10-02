4f19495a4033a9587b2564497910bdf3f3c74c30
Matt Nihiser <mattnihiser@gmail.com>
2019-10-02 17:27:31 -0400

    Update App.js
    Added Apollo to pull in data. using as a ref point.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
 import "./App.scss";
 
@@ -11,56 +13,52 @@ import Spacer from "../Spacer/Spacer";
 
 const subtext = "Don't be fooled by the French name, this place oozes with Californian flair.";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <SectionWrapper>
-          <Hero subtext={subtext}>Restaurants</Hero>
-        </SectionWrapper>
-
-        <Spacer size="medium" />
-        <Divider />
-        <Spacer size="medium" />
-
-        <SectionWrapper>
-          <CardList>
-            <Card
-              headline="Restaurant 1, with a really long name, you know?"
-              rating={3}
-              category="thai"
-              price="$$$$"
-              status={true}
-            />
-
-            <Card
-              headline="Restaurant 2"
-              rating={3}
-              category="thai"
-              price="$$$$"
-              status={true}
-            />
-
-            <Card
-              headline="Restaurant 3"
-              rating={3}
-              category="thai"
-              price="$$$$"
-              status={true}
-            />
-
-            <Card
-              headline="Restaurant 4"
-              rating={3}
-              category="thai"
-              price="$$$$"
-              status={true}
-            />
-          </CardList>
-        </SectionWrapper>
-      </div>
-    )
+const RESTAURANT_LIST = gql`
+{
+  search(categories:"Food", location:"Las Vegas"){
+    business{
+      name
+      rating
+      price
+      photos
+    }
   }
+}
+`;
+
+const App = () => {
+  const {loading, error, data } = useQuery(RESTAURANT_LIST);
+
+  if(loading) return <p>Loading!!</p>;
+  if(error) return <p>ERROR!</p>;
+
+  return (
+    <div className="App">
+      <SectionWrapper>
+        <Hero subtext={subtext}>Restaurants</Hero>
+      </SectionWrapper>
+
+      <Spacer size="medium" />
+      <Divider />
+      <Spacer size="medium" />
+
+      <SectionWrapper>
+        <CardList> 
+        {data.search.business.map(({name, rating, photos, price, i}) => (
+          <Card
+            image={photos}
+            name={name}
+            rating={rating}
+            category="thai"
+            price={price}
+            status={true}
+            key={i}
+          />
+        ))}
+        </CardList>
+      </SectionWrapper>
+    </div>
+  );
 }
 
 export default App;
