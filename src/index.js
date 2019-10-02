644951fa521ca5846f8988384dca915acde4b890
Matt Nihiser <mattnihiser@gmail.com>
2019-10-02 17:24:50 -0400

    Update index.js
    Adding provider and making client call

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import App from "./components/App/App";
 
 import "./scss/main.scss";
 
-const Root = (
-  // This will change over time for Provider
-  <App />
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/https://api.yelp.com/v3/graphql',
+  headers: {
+    authorization: `Bearer ${process.env.YELP_KEY}`,
+    "Accept-Language": 'en-US'
+  }
+});
+
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
 
-ReactDOM.render(Root, document.getElementById('root'));
+render(<Root />, document.getElementById('root'));
