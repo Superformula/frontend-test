b418eb93f083f35f6b2730909b7bfd63327388c8
Matt Nihiser <mattnihiser@gmail.com>
2019-10-08 02:44:39 -0400

    Updated CardList for queries, state, styles
    This was previously being rendered in App, but it would reload the entire page. Much easier to manage now.

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,17 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {gql} from 'apollo-boost';
+import {useQuery} from '@apollo/react-hooks';
 import PropTypes from "prop-types";
-import "./CardList.scss";
+import useGlobalHook from '../../hooks/globalHooks';
 
-const CardList = props => {
-  return (
+import Card from "../Card/Card";
+
+import "./CardList.scss";
+import Heading from "../Heading/Heading";
+import Loading from "../Loading/Loading";
+import SectionWrapper from "../SectionWrapper/SectionWrapper";
+
+const RESTAURANT_LIST = gql`
+  query Restaurants($limit:Int, $categories:String $price:String, $is_open_now:Boolean){
+    search(categories:$categories, location:"Las Vegas", limit:$limit, price:$price, open_now:$is_open_now){
+      business{
+        id
+        name
+        rating
+        price
+        photos
+        categories{
+          title
+        }
+        hours {
+          is_open_now
+        }
+      }
+      total
+    }
+  }
+`;
+
+const CardList = () => {
+  const [globalState, setGlobalState] = useGlobalHook()
+
+  const { loading, error, data, refetch } = useQuery(
+    RESTAURANT_LIST,
+    { 
+      variables: 
+      { 
+        limit: 16,
+      } 
+    }
+  );
+
+  useEffect(() => {
+    refetch({
+      price: globalState.price.toString(),
+      categories: globalState.categories,
+      is_open_now: globalState.is_open_now
+    })
+  })
+
+
+  if(loading) return <Loading />;
+  if(error) return `Error ${error}`;
+
+  if(data.search.total === 0) return (
+    <SectionWrapper>
+      <Heading level={2}>No Results</Heading>
+    </SectionWrapper>
+  );
+
+  return ( 
     <ul className="cardlist-list">
-      {props.children.map((child, i) => (
-        <li className="cardlist-list-item" key={i}>
-          {child}
+      {data.search.business.map(({ categories, id, hours, name, rating, photos, price }) => (
+        <li className="cardlist-list-item" key={id}>
+          <Card
+            image={photos.toString()}
+            name={name}
+            rating={rating}
+            category={categories[0].title}
+            price={price}
+            status={hours[0].is_open_now}
+            key={id}
+            id={id}
+          />
         </li>
       ))}
-    </ul>
-  );
+  </ul>
+  )
 };
 
 CardList.protoTypes = {
