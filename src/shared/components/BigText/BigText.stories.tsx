fd0b70258a09caf82b01893a823b49258166b99f
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2019-11-16 08:00:00 +0000

    Add storybook config, sample stories, install storybook packages

---
+++ src/shared/components/BigText/BigText.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { BigText } from './BigText';
+
+storiesOf('BigText', module)
+  .add('default', () => <BigText />)
