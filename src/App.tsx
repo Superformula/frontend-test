71f2860d1358056c0ece7f7976ba719cb67a3d59
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2019-11-16 18:57:46 +0100

    Add basic router separation in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
 import React from 'react';
 import './styles/core.scss';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-export const App: React.FC = () => <div>App</div>;
+import { Restaurants } from 'screens/Restaurants/Restaurants';
+import { RestaurantDetails } from 'screens/RestaurantDetails/RestaurantDetails';
+
+export const App: React.FC = () => (
+  <div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Restaurants />
+        </Route>
+        <Route path="/restaurants/:restaurantId">
+          <RestaurantDetails />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
