59b70563d6d37b764d87172e34293efd94a12e18
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2019-11-17 01:39:10 +0100

    Cut too long cuisine name, don't show dot if no priceRange provided

--- src/shared/components/CuisinePriceInfo/CuisinePriceInfo.tsx
+++ src/shared/components/CuisinePriceInfo/CuisinePriceInfo.tsx
@@ -4,6 +4,8 @@ import classNames from 'classnames';
 import { CuisinePriceInfoProps, Size } from './CuisinePriceInfo.types';
 import './CuisinePriceInfo.scss';
 
+const MAX_CUISINE_LENGTH = 17;
+
 /**
  * Displays information about cuisine and price range label
  */
@@ -17,8 +19,8 @@ export const CuisinePriceInfo: React.FC<CuisinePriceInfoProps> & { size: typeof 
       'cuisine-price-info--big': size === Size.BIG,
     })}
   >
-    {cuisineName}
-    <span className="cuisine-price-info__price">{priceRange}</span>
+    {cuisineName.length > MAX_CUISINE_LENGTH ? `${cuisineName.slice(0, MAX_CUISINE_LENGTH)}...` : cuisineName}
+    {priceRange && <span className="cuisine-price-info__price">{priceRange}</span>}
   </p>
 );
 
