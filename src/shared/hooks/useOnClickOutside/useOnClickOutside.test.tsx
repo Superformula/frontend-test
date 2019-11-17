758c78d9fe07f4f1aae524c6c08c12efd0b7a965
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2019-11-17 16:10:40 +0100

    Add useOnClickOutside tests

---
+++ src/shared/hooks/useOnClickOutside/useOnClickOutside.test.tsx
@@ -0,0 +1,49 @@
+import React, { useRef, RefObject } from 'react';
+import { waitForElement, fireEvent } from '@testing-library/dom';
+import { render, act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useOnClickOutside } from './useOnClickOutside';
+
+describe('useOnClickOutside test', () => {
+  it('fires function on click outside ref element', async () => {
+    const onClickOutsideFunction = jest.fn();
+
+    const CustomComponent = () => {
+      const ref = useRef(null);
+
+      useOnClickOutside(ref, () => {
+        onClickOutsideFunction();
+      });
+
+      return (
+        <div>
+          <div>Outside</div>
+          <div ref={ref}>Inside</div>
+        </div>
+      );
+    };
+
+    const { getByText } = render(<CustomComponent />);
+
+    await waitForElement(() => getByText('Inside'));
+
+    act(() => {
+      fireEvent.mouseDown(getByText('Inside'));
+    });
+
+    expect(onClickOutsideFunction).not.toHaveBeenCalled();
+
+    act(() => {
+      fireEvent.mouseDown(getByText('Outside'));
+    });
+
+    expect(onClickOutsideFunction).toHaveBeenCalled();
+  });
+
+  it('does nothing when ref is empty or handle is empty', () => {
+    renderHook(() => {
+      useOnClickOutside((null as unknown) as RefObject<HTMLDivElement>, () => null);
+    });
+  });
+});
