7bf546cc1e075d5538cf2fc8fa37faa96bddf585
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2019-11-18 17:10:21 +0100

    Update Review component view on different view sizes

--- src/screens/RestaurantDetails/components/Review/Review.tsx
+++ src/screens/RestaurantDetails/components/Review/Review.tsx
@@ -4,13 +4,19 @@ import './Review.scss';
 import { StarRating } from 'shared/components/StarRating/StarRating';
 import { ReviewProps } from './Review.types';
 
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+  return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+};
+
 export const Review: FC<ReviewProps> = ({ dateCreated, rating, comment, user }) => (
   <article className="review">
     <div className="review__reviewer-details">
       <img className="review__reviewer-avatar" alt="reviewer face" src={user.imageUrl} />
       <div className="review__reviewer-info">
         <p className="review__reviewer-name">{user.name}</p>
-        <p className="review__date">{dateCreated}</p>
+        <p className="review__date">{formatDate(dateCreated)}</p>
       </div>
     </div>
     <div className="review__details">
