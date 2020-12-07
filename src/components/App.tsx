0e385ed338b0d9547c6981aca42f568279180beb
rodriguezmarting <martin.rg25@hotmail.com>
2020-12-07 18:31:11 -0300

    Basic routes created.
    React router dom installed.
    Dummy Detail page created with dummy test.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home/Home";
-import Detail from "./Home/Detail/Detail";
+import Detail from "./Detail/Detail";
+
+import styles from "./App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/:id" component={Detail} />
-      </Switch>
+      <div {...styles}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/:id" component={Detail} />
+        </Switch>
+      </div>
     </Router>
   );
 };
--- src/components/Home/Detail/Detail.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const Detail: React.FC = () => {
-  return <div>Detail</div>;
-};
-
-export default Detail;
