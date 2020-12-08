f06c10868abc7afc01a4bb2de5797fb21ca009e3
rodriguezmarting <martin.rg25@hotmail.com>
2020-12-08 17:43:53 -0300

    GoogleMap component added.

--- src/components/Detail/RestaurantMedia/RestaurantMedia.tsx
+++ src/components/Detail/RestaurantMedia/RestaurantMedia.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import GoogleMapReact from "google-map-react";
 
 import styles from "./RestaurantMedia.css";
 
@@ -6,9 +7,28 @@ interface Props {
   urls: string[];
 }
 
+interface MapMarkerProps {
+  name: string;
+  lat: number;
+  lng: number;
+}
+const MapMarker: React.FC<MapMarkerProps> = ({ name }) => <div>{name}</div>;
+
 const RestaurantMedia: React.FC<Props> = ({ urls }) => {
   return (
     <section className={styles.container}>
+      <div className={styles.photo}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.GOOGLE_KEY }}
+          defaultCenter={{
+            lat: 59.95,
+            lng: 30.33,
+          }}
+          defaultZoom={11}
+        >
+          <MapMarker lat={59.955413} lng={30.337844} name={"restaurant name"} />
+        </GoogleMapReact>
+      </div>
       {urls.map((url: string) => {
         return <div key={url} className={styles.photo} />;
       })}
