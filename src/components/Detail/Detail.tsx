fc20a86521f09bdd9892d1b94237ce8e794983e2
rodriguezmarting <martin.rg25@hotmail.com>
2020-12-09 01:00:18 -0300

    Get restaurant id from url params.

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import RestaurantInfo from "../shared/RestaurantInfo/RestaurantInfo";
 import { restaurantInfoExample } from "../../declarations";
+import { useParams } from "react-router-dom";
 
 import styles from "./Detail.css";
 import RestaurantMedia from "./RestaurantMedia/RestaurantMedia";
 import Reviews from "./Reviews/Reviews";
 
 const Detail: React.FC = () => {
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div>
       <header className={styles.marginLeft}>
