d67162cac482ec31304b39325f6fd1feefd102f9
rodriguezmarting <martin.rg25@hotmail.com>
2020-12-09 13:33:47 -0300

    State management in Detail page.

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -7,38 +7,87 @@ import RestaurantMedia from "./RestaurantMedia/RestaurantMedia";
 import Reviews from "./Reviews/Reviews";
 import { IRestaurantDetail } from "../../api/yelpDeclarations";
 import { fetchRestaurant } from "src/api/yelp";
+import Spinner from "../shared/Spinner/Spinner";
 
 interface RouteParams {
   id: string;
 }
 
+export type ILoadingState = "IDLE" | "SUCCESS" | "LOADING" | "ERROR";
+
+interface IState {
+  restaurant: IRestaurantDetail;
+  loadingState: ILoadingState;
+}
+
+interface fetchRestaurantAction {
+  type: "FETCH_RESTAURANT";
+}
+
+interface fetchRestaurantSuccessAction {
+  type: "FETCH_RESTAURANT_SUCCESS";
+  restaurant: IRestaurantDetail;
+}
+
+interface fetchRestaurantErrorAction {
+  type: "FETCH_RESTAURANT_ERROR";
+}
+
+type IDetailActions =
+  | fetchRestaurantAction
+  | fetchRestaurantSuccessAction
+  | fetchRestaurantErrorAction;
+
 const Detail: React.FC = () => {
+  const initialState: IState = { restaurant: null, loadingState: "IDLE" };
+
+  const reducer = (state: IState, action: IDetailActions) => {
+    switch (action.type) {
+      case "FETCH_RESTAURANT":
+        return { ...state, loadingState: "LOADING" };
+      case "FETCH_RESTAURANT_SUCCESS":
+        return { loadingState: "SUCCESS", restaurant: action.restaurant };
+      case "FETCH_RESTAURANT_ERROR":
+        return { ...state, loadingState: "ERROR" };
+      default:
+        throw new Error("action type not found");
+    }
+  };
+
+  const [{ restaurant, loadingState }, dispatch] = React.useReducer(reducer, initialState);
   const { id } = useParams<RouteParams>();
-  const [restaurant, setRestaurant] = React.useState<IRestaurantDetail>(null);
 
   React.useEffect(() => {
     async function init() {
+      dispatch({ type: "FETCH_RESTAURANT" });
       const restaurant = await fetchRestaurant(id);
-      setRestaurant(restaurant);
+      if (restaurant) {
+        dispatch({ type: "FETCH_RESTAURANT_SUCCESS", restaurant });
+      } else {
+        dispatch({ type: "FETCH_RESTAURANT_ERROR" });
+      }
     }
 
     init();
-  });
+  }, [id]);
 
-  // TODO: add spinner
-  if (!restaurant) {
-    return <div>Loading</div>;
+  switch (loadingState) {
+    case "IDLE":
+    case "LOADING":
+      return <Spinner />;
+    case "ERROR":
+      return <div>Error</div>;
+    case "SUCCESS":
+      return (
+        <div>
+          <header className={styles.marginLeft}>
+            <RestaurantInfo variant="header" restaurant={restaurant} />
+          </header>
+          <RestaurantMedia urls={restaurant.photos} />
+          <Reviews restaurant={restaurant} />
+        </div>
+      );
   }
-
-  return (
-    <div>
-      <header className={styles.marginLeft}>
-        <RestaurantInfo variant="header" restaurant={restaurant} />
-      </header>
-      <RestaurantMedia urls={restaurant.photos} />
-      <Reviews restaurant={restaurant} />
-    </div>
-  );
 };
 
 export default Detail;
