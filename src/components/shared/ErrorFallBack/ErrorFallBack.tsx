dee71f9619cb3c25cf1ba6d020e4e4f4b29fc674
rodriguezmarting <martin.rg25@hotmail.com>
2020-12-09 23:54:19 -0300

    ErrorFallBack styles updated.

--- src/components/shared/ErrorFallBack/ErrorFallBack.tsx
+++ src/components/shared/ErrorFallBack/ErrorFallBack.tsx
@@ -5,7 +5,7 @@ import styles from "./ErrorFallBack.css";
 const ErrorFallback: React.FC<FallbackProps> = ({ error }) => {
   return (
     <div className={styles.container} role="alert">
-      <p>Something went wrong:</p>
+      <p>Something went wrong</p>
       <pre>{error?.message}</pre>
     </div>
   );
