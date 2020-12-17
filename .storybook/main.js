54cffebd2344b0ebca46d695031eea16ecd30d2a
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-12-17 03:58:08 -0300

    chore(storybook): cleaned up the storybook config
    Applied the rules from eslint to the storybook config files

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,19 +3,19 @@ const path = require('path');
 module.exports = {
   stories: [
     '../components/**/*.stories.mdx',
-    '../components/**/*.stories.@(js|jsx|ts|tsx)'
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
+    '@storybook/addon-essentials',
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../')
+      include: path.resolve(__dirname, '../'),
     });
 
     return config;
-  }
+  },
 };
