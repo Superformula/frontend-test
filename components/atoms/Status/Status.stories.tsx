7d26777d561304b26d8c91e0a1756fb2a6e80554
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-12-19 02:00:55 -0300

    chore(atom): created the status story

---
+++ components/atoms/Status/Status.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import { Status, StatusProps } from './Status';
+
+export default {
+  title: 'atoms/Status',
+  component: Status,
+} as Meta;
+
+const Template: Story<StatusProps> = (args) => <Status {...args} />;
+
+export const Open = Template.bind({});
+Open.args = {
+  variant: 'open',
+};
+
+export const Closed = Template.bind({});
+Closed.args = {
+  variant: 'close',
+};
