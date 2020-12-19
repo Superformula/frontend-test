f5e23edc89b7ccdc384f97f1391607a552fcd9c9
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-12-19 01:55:44 -0300

    chore(atom): updated the typography structure
    Implemented a generic component to render html elements

--- components/atoms/Typography/Typography.tsx
+++ components/atoms/Typography/Typography.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { createElement, FC, HTMLAttributes } from 'react';
 import classNames from 'classnames';
 
 import styles from './Typography.module.scss';
@@ -7,6 +7,16 @@ export interface TypographyProps {
   variant: 'title' | 'subtitle' | 'headline' | 'body' | 'status' | 'label';
 }
 
+interface TextProps extends HTMLAttributes<HTMLElement> {
+  element: 'h1' | 'h2' | 'h3' | 'p';
+}
+
+const Text: FC<TextProps> = ({
+  element,
+  children,
+  ...props
+}) => createElement(element, props, children);
+
 export const Typography: FC<TypographyProps> = ({ variant, children }) => {
   const elementClass = classNames({
     [styles.typography]: true,
@@ -14,9 +24,9 @@ export const Typography: FC<TypographyProps> = ({ variant, children }) => {
   });
 
   switch (variant) {
-    case 'title': return <h1 className={elementClass}>{children}</h1>;
-    case 'subtitle': return <h2 className={elementClass}>{children}</h2>;
-    case 'headline': return <h3 className={elementClass}>{children}</h3>;
-    default: return <p className={elementClass}>{children}</p>;
+    case 'title': return <Text element="h1" className={elementClass}>{children}</Text>;
+    case 'subtitle': return <Text element="h2" className={elementClass}>{children}</Text>;
+    case 'headline': return <Text element="h3" className={elementClass}>{children}</Text>;
+    default: return <Text element="p" className={elementClass}>{children}</Text>;
   }
 };
