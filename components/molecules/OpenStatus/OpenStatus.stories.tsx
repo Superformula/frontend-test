6a60cc5d8ca585697faac116e6787403101fc0f9
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-12-19 06:14:12 -0300

    feat(molecule): created the open status molecule

---
+++ components/molecules/OpenStatus/OpenStatus.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import { OpenStatus, OpenStatusProps } from './OpenStatus';
+
+export default {
+  title: 'molecules/OpenStatus',
+  component: OpenStatus,
+} as Meta;
+
+const Template: Story<OpenStatusProps> = (args) => <OpenStatus {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  status: 'open',
+};
+
+export const Open = Template.bind({});
+Open.args = {
+  status: 'open',
+};
+
+export const Closed = Template.bind({});
+Closed.args = {
+  status: 'close',
+};
---
+++ components/molecules/OpenStatus/OpenStatus.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import classNames from 'classnames';
+
+import { Status, StatusProps, Typography } from '~/components/atoms';
+import styles from './OpenStatus.module.scss';
+
+export interface OpenStatusProps {
+  status?: StatusProps['variant'];
+}
+
+export const OpenStatus: FC<OpenStatusProps> = ({ status = 'open' }) => {
+  const elementClass = classNames({
+    [styles['open-status']]: true,
+  });
+
+  return (
+    <div className={elementClass}>
+      <Status variant={status} />
+      <Typography variant="status">{status}</Typography>
+    </div>
+  );
+};
