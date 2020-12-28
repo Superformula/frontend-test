a4806181a561714228cb0c0619d569ea48f508d1
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-12-28 11:52:53 -0300

    feat(page): implemented the offset in the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-/* eslint-disable no-console */
+import axios from 'axios';
 import { GetStaticProps } from 'next';
-import React, { FC, useState } from 'react';
+import Router from 'next/router';
+
+import React, {
+  FC,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 
 import {
   FilterData,
@@ -8,7 +15,10 @@ import {
   RestaurantItemData,
   SelectOption,
 } from '~/components';
+
+import { Business } from '~/models';
 import { BusinessService, CategoryService } from '~/services';
+import { useIsMount } from '~/utils/hooks';
 import { mapBusinessToMain } from '~/utils/map';
 
 interface MainPageProps {
@@ -17,44 +27,88 @@ interface MainPageProps {
 }
 
 const MainPage: FC<MainPageProps> = ({ categoryOptions = [], loadedItems = [] }) => {
-  const [items] = useState<RestaurantItemData[]>(loadedItems);
+  const isMount = useIsMount();
 
-  const priceOptions: SelectOption[] = [{
-    id: 'all',
-    text: 'All',
-  }, {
-    id: '1',
-    text: '$',
-  }, {
-    id: '2',
-    text: '$$',
-  }, {
-    id: '3',
-    text: '$$$',
-  }, {
-    id: '4',
-    text: '$$$$',
-  }];
+  const [category, setCategory] = useState<string | null>(null);
+  const [price, setPrice] = useState<string | null>(null);
+  const [openNow, setOpenNow] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [items, setItems] = useState<RestaurantItemData[]>(loadedItems);
+  const [filteredItems, setFilteredItems] = useState<RestaurantItemData[]>([]);
+
+  const priceOptions: SelectOption[] = [
+    { id: 'all', text: 'All' },
+    { id: '1', text: '$' },
+    { id: '2', text: '$$' },
+    { id: '3', text: '$$$' },
+    { id: '4', text: '$$$$' },
+  ];
+
+  const requestBusinesses = useCallback(async (append?: boolean, offset?: number) => {
+    setLoading(true);
+
+    const params = { category, offset };
+    if (category === null || category === 'all') params.category = undefined;
+
+    const { data } = await axios.get<{ businesses: Business[] }>('/api/businesses', { params });
+
+    setItems((value) => {
+      const list = mapBusinessToMain(data.businesses);
+
+      if (append) return [...value, ...list];
+      return list;
+    });
+
+    setLoading(false);
+  }, [category]);
 
   const handleItemClick = (id: string) => {
-    console.log(id);
+    Router.push(`/${id}`);
   };
 
   const handleFilterChange = (value: FilterData) => {
-    console.log(value);
+    setCategory(value.category);
+    setPrice(value.price);
+    setOpenNow(value.open);
   };
 
   const handleLoadMoreClick = () => {
-    console.log('load more');
+    requestBusinesses(true, items.length);
   };
+
+  useEffect(() => {
+    if (!isMount) requestBusinesses();
+  }, [requestBusinesses]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setFilteredItems(items.filter((item) => {
+      const isOpen = item.status === 'open';
+      const allPrices = price === 'all' || price === null;
+      const matchesPrice = item.price.toString() === price;
+
+      if (openNow) {
+        if (allPrices) return isOpen;
+        return isOpen && matchesPrice;
+      }
+
+      if (allPrices) return true;
+      return matchesPrice;
+    }));
+
+    setLoading(false);
+  }, [items, price, openNow]);
 
   return (
     <Main
+      showLoadMore
+      loading={loading}
       title="Restaurants"
       body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
       priceOptions={priceOptions}
       categoryOptions={categoryOptions}
-      items={items}
+      items={filteredItems}
       onItemClick={handleItemClick}
       onFilterChange={handleFilterChange}
       onLoadMoreClick={handleLoadMoreClick}
@@ -63,10 +117,12 @@ const MainPage: FC<MainPageProps> = ({ categoryOptions = [], loadedItems = [] })
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { categories } = await CategoryService.getAll();
-  const categoryOptions = categories.map(({ alias, title }) => ({ id: alias, text: title }));
+  const [{ categories }, { businesses }] = await Promise.all([
+    CategoryService.getAll(),
+    BusinessService.getAll(),
+  ]);
 
-  const { businesses } = await BusinessService.getAll();
+  const categoryOptions = categories.map(({ alias, title }) => ({ id: alias, text: title }));
 
   return {
     props: {
