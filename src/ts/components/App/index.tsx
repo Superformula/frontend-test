50e2f51edfc33802158b66485468605e379e1e32
jvanderen1 <jvanderen1@gmail.com>
2021-01-13 13:14:03 -0800

    Updated App component
    Signed-off-by: jvanderen1 <jvanderen1@gmail.com>

--- src/ts/components/App/index.tsx
+++ src/ts/components/App/index.tsx
@@ -3,18 +3,30 @@
 
 // Libraries
 import React from 'react';
+// Components
+import MainView from '../MainView';
+// Style
+import style from './style.module.css';
+// Temp
+import { ApolloProvider } from "@apollo/client";
+import yelpClient from "../../graphql/yelp/client";
 
 // Internal
 // --------
 
-const App: React.FC = () => {
+export interface AppProps {
+}
+
+const App: React.FC<AppProps> = ({}) => {
     //
     // Render
 
     return (
-        <div>
-            Hello World
-        </div>
+        <ApolloProvider client={ yelpClient }>
+            <div className={ style.app }>
+                <MainView/>
+            </div>
+        </ApolloProvider>
     );
 }
 
