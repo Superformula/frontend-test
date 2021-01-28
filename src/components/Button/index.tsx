14de5b7ce8d6f76b9a038681f108bc3275d48718
Fran Guijarro <franleplant@gmail.com>
2021-01-28 17:34:25 -0300

    button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,54 @@
 import styled from "@emotion/styled";
 
-export interface IProps {}
+export interface IProps {
+  fullWidth: boolean;
+  inverse: boolean;
+  disabled: boolean;
+}
 
-const Button = styled.button`
-  background-color: ${(p) => p.theme.backgroundColor};
-  width: 300px;
-`;
+const Button = styled.button<IProps>(
+  ({ theme, fullWidth, inverse, disabled }) => {
+    // TODO extract this into a function that
+    // can be super easily be unit tested
+    let bgColor = theme.colors.primary;
+    let fgColor = theme.colors.white;
+    let borderColor = theme.colors.primary;
+
+    if (disabled) {
+      bgColor = theme.colors.grey100;
+      fgColor = theme.colors.grey200;
+      borderColor = theme.colors.grey100;
+    }
+
+    if (inverse) {
+      bgColor = theme.colors.white;
+      fgColor = theme.colors.primary;
+      borderColor = theme.colors.primary;
+
+      if (disabled) {
+        bgColor = theme.colors.white;
+        fgColor = theme.colors.grey200;
+        borderColor = theme.colors.grey100;
+      }
+    }
+
+    return `
+    background-color: ${bgColor};
+    color: ${fgColor};
+    border: 1px solid ${borderColor};
+    border-radius: ${theme.borderRadius.small};
+    padding: 16px;
+    text-align: center;
+    font-size: ${theme.fontSize.x400};
+    text-transform: uppercase;
+    cursor: pointer;
+    ${fullWidth ? `width: 100%;` : ``}
+
+    &:hover {
+      background-color: ${theme.colors.primary}cc;
+    }
+  `;
+  }
+);
 
 export default Button;
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,26 @@
 export const theme = {
+  colors: {
+    primary: "#002B56",
+    blue: "#002B56",
+    green: "#00E8A4",
+    fuchsia: "#FF3548",
+    white: "#FFFFFF",
+    grey100: "#E6E6E6",
+    grey200: "#C8C8C8",
+    grey300: "#D8D8D8",
+    grey400: "#606060",
+    grey500: "#666666",
+    grey600: "#333333",
+  },
+  // TODO not so sure about this
+  borderRadius: {
+    small: "2px",
+  },
+  fontSize: {
+    x300: "12px",
+    x400: "14px",
+  },
   backgroundColor: "green",
-  color: "red",
 };
 
 declare module "@emotion/react" {
