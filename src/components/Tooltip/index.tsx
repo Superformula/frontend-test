780c6c83ae457959e974099e0d8aa13b03d1fd2f
Fran Guijarro <franleplant@gmail.com>
2021-01-30 12:48:32 -0300

    Tooltip: docs

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -13,6 +13,23 @@ export interface IProps {
   children: (props: IRenderChildrenProps) => React.ReactNode | null;
 }
 
+/**
+ * A simple implementation of a tooltip.
+ *
+ * It has some limitations but mainly due to
+ * lack of time to solve them (i.e. not adapting to screen changes),
+ * but it is a really simple but powerful way of
+ * building a tooltip mainly due to Fragments and Portals.
+ * By using Portals I can easily make the tooltip
+ * a child of the document.body and this in turn allows
+ * the tooltip to require basically nothing from the
+ * TooltipTrigger (i.e. a button, a text, whatever).
+ *
+ * @dev a nice real world use case for portals plus
+ * a nice way of showing off how good react compoisiton is
+ *
+ *
+ */
 export default function Tooltip(props: IProps) {
   const [triggerBoundingRect, setTriggerBoundingRect] = useState<
     DOMRect | undefined
